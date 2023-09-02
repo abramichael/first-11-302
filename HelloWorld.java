244839c5dbdcc00bbcf8752db9fc8f0675c8a65e
abramichael <abramskiy@gmail.com>
2023-09-02 13:26:46 +0300

    Added word welcome

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 	
 	public static void main(String [] args) {
 		
-		System.out.println("Hello, Dear ITIS 11-302!");
+		System.out.println("Hello, Dear ITIS 11-302! Welcome!");
 		
 	}
 	
